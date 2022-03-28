9bb293ac407226a4ed232f9fb86f4af6c48b702c
TaraCarette <terra.the.great@gmail.com>
2022-03-28 21:35:37 +0200

    Code capable of calculating auc for task 1

---
+++ negative-selection/language_auc.py
@@ -0,0 +1,33 @@
+import numpy as np
+from sklearn import metrics
+
+dataFile = "combined.test"
+scoreFile = "scores.txt"
+numEng = 123
+
+
+# associate the data with the scores
+with open("combined.test") as f:
+    data = f.read().splitlines()
+
+with open("scores.txt") as f:
+    scores = f.read().splitlines()
+
+# put all data in list with score and flag is english or not
+scoreData = []
+for i in range(0, len(data)):
+    if i <= numEng:
+        scoreData.append((data[i], float(scores[i]), 1))
+    else:
+        scoreData.append((data[i], float(scores[i]), 2))
+
+# sort the data by score
+scoreData.sort(key=lambda tup: tup[1])
+
+sortedLabels = np.array([l[2] for l in scoreData])
+sortedScores = np.array([l[1] for l in scoreData])
+
+# calculate the curve
+fpr, tpr, thresholds = metrics.roc_curve(sortedLabels, sortedScores, pos_label=2)
+# then calculate the auc
+print(metrics.auc(fpr, tpr))
